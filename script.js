8c534156a6ad0df1ad4f529ecbe343e51de2156d
Thiago Medeiros <thiagoodemedeiros@gmail.com>
2025-07-13 15:17:07 -0300

    feat: first part of js in the app

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form");
+
+const apiKeyInput = document.getElementById("apiKey");
+const gameSelect = document.getElementById("gameSelect");
+const questionInput = document.getElementById("questionInput");
+const askButton = document.getElementById("askButton");
+
+const aiResponse = document.getElementById("aiResponse");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
