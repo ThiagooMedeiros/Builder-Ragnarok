045bcb5f6a4f5daa7ecc2522604a285b4bc35ce2
Thiago Medeiros <thiagoodemedeiros@gmail.com>
2025-07-27 15:51:55 -0300

    feat: prompt engineering

--- script.js
+++ script.js
@@ -18,11 +18,31 @@ const askAI = async (question, game, apiKey) => {
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
 
   const pergunta = ` 
-  Olha, tenho esse jogo ${game} e queria saber de forma resumida ${question}
+    ## Especialidade
+    - Você é um especialista assistente de meta para o jogo ${game}.  
+
+    ## Tarefa
+    - Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds e dicas.
+
+    ## Regras 
+    - Se você não sabe a resposta, responda com "Não sei" e não tente inventar uma resposta.
+    - Considere a data atual para a sua resposta ${new Date().toLocaleDateString()}.
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responda itens que você não tenha certeza que existe no patch atual.
+    - Se a pergunta não está relacionada ao jogo responda com "Essa pergunta não está relacionada ao jogo".
+
+    ## Resposta 
+    - Economize na resposta, seja direto.
+    - Responda em markdown.
+    - Não faça nenhuma saudação ou despedida na sua resposta, apenas responda a pergunta feita.
+
+    ---
+    Aqui está a pergunta do usuário: ${question}.
   `;
 
   const contents = [
     {
+      role: "user",
       parts: [
         {
           text: pergunta,
@@ -31,6 +51,11 @@ const askAI = async (question, game, apiKey) => {
     },
   ];
 
+  const tools = [
+    {
+      google_search: {},
+    },
+  ];
   //chamada
 
   const response = await fetch(geminiURL, {
@@ -40,6 +65,7 @@ const askAI = async (question, game, apiKey) => {
     },
     body: JSON.stringify({
       contents,
+      tools,
     }),
   });
 
