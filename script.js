3cf79dda23a7a1a1d30be3197e0c26af8ce9a8ff
Thiago Medeiros <thiagoodemedeiros@gmail.com>
2025-07-26 23:55:15 -0300

    feat: function to call the AI

--- script.js
+++ script.js
@@ -8,18 +8,48 @@ const askButton = document.getElementById("askButton");
 const aiResponse = document.getElementById("aiResponse");
 
 const askAI = async (question, game, apiKey) => {
-  const model = "gemini-2.0-flash";
-  const geminiURL = ``;
+  const model = "gemini-2.5-flash";
+  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+
+  const pergunta = ` 
+  Olha, tenho esse jogo ${game} e queria saber o ${question}
+  `;
+
+  const contents = [
+    {
+      parts: [
+        {
+          text: pergunta,
+        },
+      ],
+    },
+  ];
+
+  //chamada
+
+  const response = await fetch(geminiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents,
+    }),
+  });
+
+  const data = await response.json();
+
+  console.log({ data });
+
+  return;
 };
 
-const submitForm = (event) => {
+const submitForm = async (event) => {
   event.preventDefault();
 
   const apiKey = apiKeyInput.value;
   const game = gameSelect.value;
   const question = questionInput.value;
-
-  console.log({ apiKey, game, question });
 
   if (apiKey == "" || game == "" || question == "") {
     alert("Preencha todos os campos");
@@ -32,7 +62,7 @@ const submitForm = (event) => {
 
   try {
     // perguntar para ia
-    askAI(question, game, apiKey);
+    await askAI(question, game, apiKey);
   } catch (error) {
     console.log("Erro: ", error);
   } finally {
